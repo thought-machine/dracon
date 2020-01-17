1f5d2b13aa76712de85589090067a11011e7baab
VJ Patel <vj@vjpatel.me>
2020-01-17 22:27:59 +0000

    fix tests
    Signed-off-by: VJ Patel <vj@vjpatel.me>

--- pkg/kubernetes/apply_test.go
+++ pkg/kubernetes/apply_test.go
@@ -1,27 +1,26 @@
-package kubernetes_test
+package kubernetes
 
 import (
 	"strings"
 	"testing"
 
-	"github.com/thought-machine/dracon/pkg/kubernetes"
-	"github.com/thought-machine/dracon/plz-out/go/src/github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetCmd(t *testing.T) {
 	var tests = []struct {
 		desc   string
-		inOpts kubernetes.KubectlOpts
+		inOpts KubectlOpts
 		outCmd string
 	}{
-		{"none", kubernetes.KubectlOpts{}, "kubectl apply -f -"},
-		{"namespace", kubernetes.KubectlOpts{Namespace: "default"}, `kubectl apply -f - --namespace=default`},
-		{"context", kubernetes.KubectlOpts{Context: "minikube"}, `kubectl apply -f - --context=minikube`},
-		{"namespace&context", kubernetes.KubectlOpts{Context: "minikube", Namespace: "default"}, `kubectl apply -f - --context=minikube --namespace=default`},
+		{"none", KubectlOpts{}, "kubectl apply -f -"},
+		{"namespace", KubectlOpts{Namespace: "default"}, `kubectl apply -f - --namespace=default`},
+		{"context", KubectlOpts{Context: "minikube"}, `kubectl apply -f - --context=minikube`},
+		{"namespace&context", KubectlOpts{Context: "minikube", Namespace: "default"}, `kubectl apply -f - --context=minikube --namespace=default`},
 	}
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
-			outCmd := strings.Join(kubernetes.GetCmd(&tt.inOpts), " ")
+			outCmd := strings.Join(GetCmd(&tt.inOpts), " ")
 			assert.Equal(t, tt.outCmd, outCmd)
 		})
 	}
