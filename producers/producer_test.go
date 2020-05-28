0008e43ec66ffdd25cbbdb8c7c7085f5d494e402
Spyros <northdpole@users.noreply.github.com>
2020-05-28 17:45:48 +0100

    using io.Reader for ParseInFileJSON (#29)
    * using io.Reader for ParseInFileJSON
    
    * added tests

--- producers/producer_test.go
+++ producers/producer_test.go
@@ -12,6 +12,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testJ struct {
+	Foo string
+}
+
+func TestParseInFileJSON(t *testing.T) {
+	file, err := ioutil.TempFile("/tmp", "dracon-in")
+	assert.Nil(t, err)
+
+	testJSON := `{"Foo":"bar"}`
+	defer os.Remove(file.Name())
+	bytes, err := file.WriteString(testJSON)
+	assert.Nil(t, err)
+	assert.Equal(t, bytes, 13)
+	InResults = file.Name()
+
+	var inJSON testJ
+	assert.Nil(t, ParseInFileJSON(&inJSON))
+	assert.Equal(t, inJSON.Foo, "bar")
+}
+
 func TestWriteDraconOut(t *testing.T) {
 	tmpFile, err := ioutil.TempFile("", "dracon-test")
 	assert.Nil(t, err)
