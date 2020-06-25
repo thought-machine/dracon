ea214edd7701c13bdd85103fec4ac955e4f1f4df
Spyros <northdpole@users.noreply.github.com>
2020-06-25 16:19:48 +0100

    npm audit producer (#38)

---
+++ producers/npm_audit/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	v1 "api/proto/v1"
+	types "types/npmaudit-issue"
+
+	"fmt"
+	"log"
+	"producers"
+	"strings"
+)
+
+func main() {
+	if err := producers.ParseFlags(); err != nil {
+		log.Fatal(err)
+	}
+	var results types.NpmAuditOut
+	if err := producers.ParseInFileJSON(&results); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := producers.WriteDraconOut(
+		"npm-audit",
+		parseOut(&results),
+	); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parseOut(results *types.NpmAuditOut) []*v1.Issue {
+	issues := []*v1.Issue{}
+	for _, res := range results.Advisories {
+		issues = append(issues, parseResult(&res))
+	}
+	return issues
+}
+func parseResult(r *types.NpmAuditAdvisories) *v1.Issue {
+	return &v1.Issue{
+		Target:   r.ModuleName,
+		Type:     "Vulnerable Dependency",
+		Title:    r.Title,
+		Severity: v1.Severity(v1.Severity_value[fmt.Sprintf("SEVERITY_%s", strings.ToUpper(r.Severity))]),
+		// Cvss:        0.0,
+		Confidence: v1.Confidence_CONFIDENCE_HIGH,
+		Description: fmt.Sprintf("Vulnerable Versions: %s\nRecommendation: %s\nOverview: %s\nReferences: %s\nNPM Advisory URL: %s\n",
+			r.VulnerableVersions, r.Recommendation, r.Overview, r.References, r.URL),
+	}
+}
