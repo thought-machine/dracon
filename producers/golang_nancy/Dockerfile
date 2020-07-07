b5deb97d7b60bd241d36bfb8362a535d1b2dad7f
Spyros <morfeas3000@gmail.com>
2020-07-07 14:05:59 +0100

    introducing nancy for golang third party dependency checking

---
+++ producers/golang_nancy/Dockerfile
@@ -0,0 +1,5 @@
+FROM //build/docker:dracon-base-go
+
+COPY nancy /parse
+
+ENTRYPOINT ["/parse"]
---
+++ producers/golang_nancy/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	v1 "api/proto/v1"
+	"strconv"
+	types "types/nancy-issue"
+
+	"fmt"
+	"log"
+	"producers"
+)
+
+func main() {
+	if err := producers.ParseFlags(); err != nil {
+		log.Fatal(err)
+	}
+	var results types.NancyOut
+	if err := producers.ParseInFileJSON(&results); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := producers.WriteDraconOut(
+		"nancy",
+		parseOut(&results),
+	); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parseOut(results *types.NancyOut) []*v1.Issue {
+	issues := []*v1.Issue{}
+	for _, res := range results.Vulnerable {
+		target := res.Coordinates
+		for _, vuln := range res.Vulnerabilities {
+			issues = append(issues, parseResult(&vuln, target))
+		}
+	}
+	return issues
+}
+
+func cvssToSeverity(score string) v1.Severity {
+
+	switch s, err := strconv.ParseFloat(score, 64); err == nil {
+	case 0.1 <= s && s <= 3.9:
+		return v1.Severity_SEVERITY_LOW
+	case 4.0 <= s && s <= 6.9:
+		return v1.Severity_SEVERITY_MEDIUM
+	case 7.0 <= s && s <= 8.9:
+		return v1.Severity_SEVERITY_HIGH
+	case 9.0 <= s && s <= 10.0:
+		return v1.Severity_SEVERITY_CRITICAL
+	default:
+		return v1.Severity_SEVERITY_INFO
+
+	}
+}
+func parseResult(r *types.NancyVulnerabilities, target string) *v1.Issue {
+	cvss, err := strconv.ParseFloat(r.CvssScore, 64)
+	if err != nil {
+		cvss = 0.0
+	}
+	return &v1.Issue{
+		Target:     target,
+		Type:       "Vulnerable Dependency",
+		Title:      r.Title,
+		Severity:   cvssToSeverity(r.CvssScore),
+		Confidence: v1.Confidence_CONFIDENCE_HIGH,
+		Cvss:       cvss,
+		Description: fmt.Sprintf("CVSS Score: %v\nCvssVector: %s\nCve: %s\nCwe: %s\nReference: %s\n",
+			r.CvssScore, r.CvssVector, r.Cve, r.Cwe, r.Reference),
+	}
+}
