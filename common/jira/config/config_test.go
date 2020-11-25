89a71515887b56c66a7eaedd41272f2da11ef767
Spyros <morfeas3000@gmail.com>
2020-11-25 17:16:45 +0000

    jira library

--- common/jira/config/config_test.go
+++ common/jira/config/config_test.go
@@ -9,7 +9,7 @@ import (
 
 var sampleConfig = Config{
 	DefaultValues: DefaultValues{
-		Project:         "TOY",
+		Project:         "TEST",
 		IssueType:       "Vulnerability",
 		Components:      []string{"c1", "c2", "c3"},
 		AffectsVersions: []string{"V1", "V2"},
@@ -26,34 +26,41 @@ var sampleConfig = Config{
 		FieldType:   "float",
 	}},
 	DescriptionExtras: []string{"target", "tool_name"},
+	SyncMappings: []JiraToDraconVulnMappings{
+		{
+			JiraStatus:     "Test",
+			JiraResolution: "Test",
+			DraconStatus:   "Resolved",
+		},
+	},
 }
 
 func TestGetConfig(t *testing.T) {
 	testConfig := `
 defaultValues:
-  project: 'TOY'
+  project: 'TEST'
   issueType: 'Vulnerability'
   components: ['c1', 'c2', 'c3']
   affectsVersions: ['V1', 'V2']
-
   customFields:
     - id: 'customfield_10000'
-      fieldType: multi-value
+      fieldType: "multi-value"
       values: ['foo', 'bar']
-
 addToDescription:
   - target
   - tool_name
-
 mappings:
-  - draconField: cvss
-    jiraField: customfield_10001
-    fieldType: float
-`
+  - draconField: "cvss"
+    jiraField: "customfield_10001"
+    fieldType: "float"
 
+syncMappings:
+  - jiraStatus: "Test"
+    jiraResolution: "Test"
+    draconStatus: "Resolved"
+`
 	reader := strings.NewReader(testConfig)
 	res, err := New(reader)
-
-	assert.NoError(t, err)
+	assert.Nil(t, err)
 	assert.EqualValues(t, res, sampleConfig)
 }
--- common/jira/config/types.go
+++ common/jira/config/types.go
@@ -2,9 +2,10 @@ package config
 
 // Config contains all the data parsed from the conf.yaml file
 type Config struct {
-	DefaultValues     DefaultValues `yaml:"defaultValues"`
-	Mappings          []Mappings    `yaml:"mappings"`
-	DescriptionExtras []string      `yaml:"addToDescription"`
+	DefaultValues     DefaultValues              `yaml:"defaultValues"`
+	Mappings          []Mappings                 `yaml:"mappings"`
+	DescriptionExtras []string                   `yaml:"addToDescription"`
+	SyncMappings      []JiraToDraconVulnMappings `yaml:"syncMappings"`
 }
 
 type CustomField struct {
@@ -26,4 +27,18 @@ type Mappings struct {
 	DraconField string `yaml:"draconField"`
 	JiraField   string `yaml:"jiraField"`
 	FieldType   string `yaml:"fieldType"`
+}
+
+// JiraToDraconVulnMappings used by the sync utiity,
+// this Mapping matches DraconStatus-es to combinations of JiraStatus and JiraResolution, look in the sample config file for examples
+// supported DraconStatus values:
+// * FalsePositive <-- will set the issue's FalsePositive flag to True
+// * Duplicate <-- if the issue already exists in the database, will do nothing, otherwise will insert a new one
+// * Resolved <-- will _REMOVE_ the finding from the database
+// JiraStatus will be matched as a string
+// JiraResolution will be matched as a string
+type JiraToDraconVulnMappings struct {
+	JiraStatus     string `yaml:"jiraStatus"`
+	JiraResolution string `yaml:"jiraResolution"`
+	DraconStatus   string `yaml:"draconStatus"`
 }
