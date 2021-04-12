9fe027334f3550b5351436a97d908057bc581cea
VJ Patel <vj@vjpatel.me>
2021-04-12 11:42:52 +0100

    Add support for E2E against multiple k8s versions

--- scripts/development/kind/setup.sh
+++ scripts/development/kind/setup.sh
@@ -3,13 +3,24 @@
 set -Eeuo pipefail
 
 DEFINE_string 'kind_cluster' 'dracon' 'Kind cluster to use' 'c'
+DEFINE_string 'kubernetes_version' '1.17' 'Kubernetes version to use' 'k'
 FLAGS "$@" || exit $?
 eval set -- "${FLAGS_ARGV}"
+
+kubernetes_version="${FLAGS_kubernetes_version//./_}"
+
+declare -A KUBERNETES_VERSIONS
+KUBERNETES_VERSIONS=(
+  ["1_16"]="kindest/node:v1.16.15@sha256:c10a63a5bda231c0a379bf91aebf8ad3c79146daca59db816fb963f731852a99"
+  ["1_17"]="kindest/node:v1.17.17@sha256:7b6369d27eee99c7a85c48ffd60e11412dc3f373658bc59b7f4d530b7056823e"
+  ["1_18"]="kindest/node:v1.18.15@sha256:5c1b980c4d0e0e8e7eb9f36f7df525d079a96169c8a8f20d8bd108c0d0889cc4"
+)
 
 kubernetes_context="kind-${FLAGS_kind_cluster}"
 
 if ! $KIND_BIN get clusters | grep "${FLAGS_kind_cluster}" 2>&1 > /dev/null; then
-  util::info "Creating KinD cluster ${FLAGS_kind_cluster}"
+  util::info "Creating KinD cluster ${FLAGS_kind_cluster} (${FLAGS_kubernetes_version})"
+  sed -i "s#___KIND_IMAGE___#${KUBERNETES_VERSIONS[$kubernetes_version]}#" "${KIND_CONFIG}"
   $KIND_BIN create cluster --name dracon --config "${KIND_CONFIG}"
   util::success "Created KinD cluster"
   kubectl config use-context "${kubernetes_context}"
