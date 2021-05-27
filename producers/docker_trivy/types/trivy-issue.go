3287594785fdd4ce98c4321ef83aca36044d557e
Spyros <morfeas3000@gmail.com>
2021-05-27 15:41:16 +0100

    trivy producer

---
+++ producers/docker_trivy/types/trivy-issue.go
@@ -0,0 +1,33 @@
+package types
+
+// TrivyOut represents the output of a trivy run that we care about
+type TrivyOut struct {
+	Vulnerable []TrivyVulnerability `json:"Vulnerabilities"`
+	Target     string
+	Type       string
+}
+
+// TrivyVulnerability represents a trivy vulnerability section with only the fields that we care about
+type TrivyVulnerability struct {
+	CVE              string    `json:"VulnerabilityID"`
+	PkgName          string    `json:"PkgName"`
+	InstalledVersion string    `json:"InstalledVersion"`
+	FixedVersion     string    `json:"FixedVersion"`
+	PrimaryURL       string    `json:"PrimaryURL"`
+	Title            string    `json:"Title"`
+	Description      string    `json:"Description"`
+	Severity         string    `json:"Severity"`
+	CweIDs           []string  `json:"CweIds"`
+	CVSS             TrivyCVSS `json:"CVSS"`
+}
+
+// TrivyCVSS wraps Trivy CVSS info struct
+type TrivyCVSS struct {
+	Nvd TrivyNvd `json:"nvd"`
+}
+
+// TrivyNvd wraps Trivy Nvd structure inside the CVSS struct
+type TrivyNvd struct {
+	V3Vector string `json:"V3Vector"`
+	V3Score  float64    `json:"V3Score"`
+}
