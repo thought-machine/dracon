6118e931c5224c87125c9ee9589a3023a3ab31ae
maria <maria@thoughtmachine.net>
2021-06-10 14:15:38 +0100

    Address comments

--- producers/zap_producer/main.go
+++ producers/zap_producer/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-
-	v1 "github.com/thought-machine/dracon/api/proto/v1"
-	"github.com/thought-machine/dracon/producers/zap_producer/types"
-
 	"fmt"
 	"log"
 
+	v1 "github.com/thought-machine/dracon/api/proto/v1"
 	"github.com/thought-machine/dracon/producers"
-
+	"github.com/thought-machine/dracon/producers/zap_producer/types"
 )
 
 func main() {
@@ -27,20 +24,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := producers.WriteDraconOut(
-		"zap",
-		parseOut(&results),
-	); err != nil {
+	if err := producers.WriteDraconOut("zap", parseOut(&results)); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func parseOut(results *types.ZapOut) []*v1.Issue {
 	issues := []*v1.Issue{}
-	for _, res := range results.Site{
-		target:= res.Name
+	for _, res := range results.Site {
+		target := res.Name
 		for _, alert := range res.Alerts {
-			issues = append (issues, parseIssue(&alert, target))
+			issues = append(issues, parseIssue(&alert, target))
 		}
 	}
 	return issues
@@ -48,72 +42,66 @@ func parseOut(results *types.ZapOut) []*v1.Issue {
 
 //zap doesn't provide cvss so assigned as 0.0
 func parseIssue(r *types.ZapAlerts, target string) *v1.Issue {
-	var cvss = 0.0
+	cvss := 0.0
 	return &v1.Issue{
-		Target:     target,
-		Type:       r.CweId,
-		Title:      r.Name ,
-		Severity:   riskcodeToSeverity(r.RiskCode),
-		Confidence: zapconfidenceToConfidence(r.Confidence),
-		Cvss:       cvss,
+		Target:      target,
+		Type:        r.CweId,
+		Title:       r.Name,
+		Severity:    riskcodeToSeverity(r.RiskCode),
+		Confidence:  zapconfidenceToConfidence(r.Confidence),
+		Cvss:        cvss,
 		Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n", r.Description, r.Solution, r.Reference),
 	}
 }
 
-//riskcode values are 0-INFO,1-LOW,2-MEDIUM,3-HIGH only available from ZAP. It is determined by the ZAP contributors 
+//riskcode values are 0-INFO,1-LOW,2-MEDIUM,3-HIGH only available from ZAP. It is determined by the ZAP contributors
 func riskcodeToSeverity(riskcode string) v1.Severity {
 
-	if (riskcode == "0") {
+	switch riskcode {
+	case "0":
 
 		return v1.Severity_SEVERITY_INFO
 
-	} else if (riskcode == "1") {
+	case "1":
 
 		return v1.Severity_SEVERITY_LOW
 
-	} else if (riskcode == "2") {
+	case "2":
 
 		return v1.Severity_SEVERITY_MEDIUM
 
-	} else if (riskcode == "3") {
+	case "3":
 
 		return v1.Severity_SEVERITY_HIGH
 
-	} else if (riskcode == "4") {
+	default:
 
 		return v1.Severity_SEVERITY_CRITICAL
-
-	} else {
-
-		return v1.Severity_SEVERITY_CRITICAL
-	}	
+	}
 }
 
-//Confidence values are 0-INFO,1-LOW,2-MEDIUM,3-HIGH only available from ZAP. It is determined by the ZAP contributors 
+//Confidence values are 0-INFO,1-LOW,2-MEDIUM,3-HIGH only available from ZAP. It is determined by the ZAP contributors
 func zapconfidenceToConfidence(confidence string) v1.Confidence {
 
-		if (confidence == "0") {
-	
-			return v1.Confidence_CONFIDENCE_INFO
-	
-		} else if (confidence == "1") {
-	
-			return v1.Confidence_CONFIDENCE_LOW
-	
-		} else if (confidence == "2") {
-	
-			return v1.Confidence_CONFIDENCE_MEDIUM
-	
-		} else if (confidence == "3") {
-	
-			return v1.Confidence_CONFIDENCE_HIGH
-	
-		} else if (confidence == "4") {
+	switch confidence {
+	case "0":
 
-			return v1.Confidence_CONFIDENCE_CRITICAL
-	
-		} else {
-			
-			return v1.Confidence_CONFIDENCE_CRITICAL
-		}	
+		return v1.Confidence_CONFIDENCE_INFO
+
+	case "1":
+
+		return v1.Confidence_CONFIDENCE_LOW
+
+	case "2":
+
+		return v1.Confidence_CONFIDENCE_MEDIUM
+
+	case "3":
+
+		return v1.Confidence_CONFIDENCE_HIGH
+
+	default:
+
+		return v1.Confidence_CONFIDENCE_CRITICAL
 	}
+}
--- producers/zap_producer/main_test.go
+++ producers/zap_producer/main_test.go
@@ -1,148 +1,140 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"testing"
 
 	v1 "github.com/thought-machine/dracon/api/proto/v1"
 	"github.com/thought-machine/dracon/producers/zap_producer/types"
-
-    "fmt"
-	"encoding/json"
-    "testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 var riskcodetests = []struct {
-    zapriskcode  string
-    severityissue v1.Severity
+	zapriskcode   string
+	severityissue v1.Severity
 }{
-    {"0", v1.Severity_SEVERITY_INFO},
-    {"1", v1.Severity_SEVERITY_LOW},
-    {"2", v1.Severity_SEVERITY_MEDIUM},
-    {"3", v1.Severity_SEVERITY_HIGH},
-    {"4", v1.Severity_SEVERITY_CRITICAL},
-    {"5", v1.Severity_SEVERITY_CRITICAL},
+	{"0", v1.Severity_SEVERITY_INFO},
+	{"1", v1.Severity_SEVERITY_LOW},
+	{"2", v1.Severity_SEVERITY_MEDIUM},
+	{"3", v1.Severity_SEVERITY_HIGH},
+	{"4", v1.Severity_SEVERITY_CRITICAL},
+	{"5", v1.Severity_SEVERITY_CRITICAL},
 }
 
 func TestZapRiskcodeToDraconSeverityParametrized(t *testing.T) {
-    for _, riskcode := range riskcodetests {
-         assert.EqualValues(t, riskcode.severityissue, riskcodeToSeverity(riskcode.zapriskcode))
-    }
+	for _, riskcode := range riskcodetests {
+		assert.EqualValues(t, riskcode.severityissue, riskcodeToSeverity(riskcode.zapriskcode))
+	}
 }
 
 var confidencetests = []struct {
-    zapconfidence  string
-    confidenceissue v1.Confidence
+	zapconfidence   string
+	confidenceissue v1.Confidence
 }{
-    {"0", v1.Confidence_CONFIDENCE_INFO},
-    {"1", v1.Confidence_CONFIDENCE_LOW},
-    {"2", v1.Confidence_CONFIDENCE_MEDIUM},
-    {"3", v1.Confidence_CONFIDENCE_HIGH},
-    {"4", v1.Confidence_CONFIDENCE_CRITICAL},
-    {"5", v1.Confidence_CONFIDENCE_CRITICAL},
+	{"0", v1.Confidence_CONFIDENCE_INFO},
+	{"1", v1.Confidence_CONFIDENCE_LOW},
+	{"2", v1.Confidence_CONFIDENCE_MEDIUM},
+	{"3", v1.Confidence_CONFIDENCE_HIGH},
+	{"4", v1.Confidence_CONFIDENCE_CRITICAL},
+	{"5", v1.Confidence_CONFIDENCE_CRITICAL},
 }
 
 func TestZapConfidenceToDraconConfidenceParametrized(t *testing.T) {
-    for _, confidence := range confidencetests {
-        assert.EqualValues(t, confidence.confidenceissue, zapconfidenceToConfidence(confidence.zapconfidence))
-    }
- }
+	for _, confidence := range confidencetests {
+		assert.EqualValues(t, confidence.confidenceissue, zapconfidenceToConfidence(confidence.zapconfidence))
+	}
+}
 
 func TestZapOutputWhenOneSiteAndOneAlert(t *testing.T) {
 	var results types.ZapOut
 	err := json.Unmarshal([]byte(exampleOutput1), &results)
-	if err != nil {
-        t.Logf(err.Error())
-		t.Fail()
+	assert.NoError(t, err)
+	issues := parseOut(&results)
+	expectedIssues := []*v1.Issue{
+		&v1.Issue{
+			Target:     "https://thisisanexample.com",
+			Type:       "16",
+			Title:      "X-Content-Type-Options Header Missing",
+			Severity:   v1.Severity_SEVERITY_LOW,
+			Cvss:       0.0,
+			Confidence: v1.Confidence_CONFIDENCE_MEDIUM,
+			Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n",
+				"<p>The Anti-MIME-Sniffing header X-Content-Type-Options was not set to 'nosniff'. This allows older versions of Internet Explorer and Chrome to perform MIME-sniffing on the response body, potentially causing the response body to be interpreted and displayed as a content type other than the declared content type. Current (early 2014) and legacy versions of Firefox will use the declared content type (if one is set), rather than performing MIME-sniffing.</p>",
+				"<p>Ensure that the application/web server sets the Content-Type header appropriately, and that it sets the X-Content-Type-Options header to 'nosniff' for all web pages.</p><p>If possible, ensure that the end user uses a standards-compliant and modern web browser that does not perform MIME-sniffing at all, or that can be directed by the web application/web server to not perform MIME-sniffing.</p>",
+				"<p>http://msdn.microsoft.com/en-us/library/ie/gg622941%28v=vs.85%29.aspx</p><p>https://owasp.org/www-community/Security_Headers</p>"),
+		},
 	}
-    issues := parseOut(&results)
-
-    expectedIssues := make([]*v1.Issue, 1)
-	expectedIssues[0] = &v1.Issue{
-		Target:     "https://thisisanexample.com",
-		Type:       "16",
-		Title:      "X-Content-Type-Options Header Missing",
-		Severity:   v1.Severity_SEVERITY_LOW,
-		Cvss:       0.0,
-		Confidence: v1.Confidence_CONFIDENCE_MEDIUM,
-        Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n",
-        "<p>The Anti-MIME-Sniffing header X-Content-Type-Options was not set to 'nosniff'. This allows older versions of Internet Explorer and Chrome to perform MIME-sniffing on the response body, potentially causing the response body to be interpreted and displayed as a content type other than the declared content type. Current (early 2014) and legacy versions of Firefox will use the declared content type (if one is set), rather than performing MIME-sniffing.</p>",
-        "<p>Ensure that the application/web server sets the Content-Type header appropriately, and that it sets the X-Content-Type-Options header to 'nosniff' for all web pages.</p><p>If possible, ensure that the end user uses a standards-compliant and modern web browser that does not perform MIME-sniffing at all, or that can be directed by the web application/web server to not perform MIME-sniffing.</p>",
-        "<p>http://msdn.microsoft.com/en-us/library/ie/gg622941%28v=vs.85%29.aspx</p><p>https://owasp.org/www-community/Security_Headers</p>"),
-	}
-    assert.Equal(t, len(expectedIssues), len(issues))
+	assert.Equal(t, len(expectedIssues), len(issues))
 
 	for _, issue := range issues {
-	 	for _, expected := range expectedIssues {
-                assert.EqualValues(t, expected.Target, issue.Target)
-                assert.EqualValues(t, expected.Type, issue.Type)
-                assert.EqualValues(t, expected.Title, issue.Title)
-	 			assert.EqualValues(t, expected.Severity, issue.Severity)
-	 			assert.EqualValues(t, expected.Cvss, issue.Cvss)
-	 			assert.EqualValues(t, expected.Confidence, issue.Confidence)
-	 			assert.EqualValues(t, expected.Description, issue.Description)
-	 	}
+		for _, expected := range expectedIssues {
+			assert.EqualValues(t, expected.Target, issue.Target)
+			assert.EqualValues(t, expected.Type, issue.Type)
+			assert.EqualValues(t, expected.Title, issue.Title)
+			assert.EqualValues(t, expected.Severity, issue.Severity)
+			assert.EqualValues(t, expected.Cvss, issue.Cvss)
+			assert.EqualValues(t, expected.Confidence, issue.Confidence)
+			assert.EqualValues(t, expected.Description, issue.Description)
+		}
 	}
 }
 
 func TestZapOutputWhenTwoSitesAndMultipleAlerts(t *testing.T) {
 	var results types.ZapOut
 	err := json.Unmarshal([]byte(exampleOutput2), &results)
-	if err != nil {
-        t.Logf(err.Error())
-		t.Fail()
+	assert.NoError(t, err)
+	issues := parseOut(&results)
+	expectedIssues := []*v1.Issue{
+		&v1.Issue{
+			Target:     "https://thisisanexample.com",
+			Type:       "16",
+			Title:      "X-Content-Type-Options Header Missing",
+			Severity:   v1.Severity_SEVERITY_LOW,
+			Cvss:       0.0,
+			Confidence: v1.Confidence_CONFIDENCE_MEDIUM,
+			Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n",
+				"<p>The Anti-MIME-Sniffing header X-Content-Type-Options was not set to 'nosniff'. This allows older versions of Internet Explorer and Chrome to perform MIME-sniffing on the response body, potentially causing the response body to be interpreted and displayed as a content type other than the declared content type. Current (early 2014) and legacy versions of Firefox will use the declared content type (if one is set), rather than performing MIME-sniffing.</p>",
+				"<p>Ensure that the application/web server sets the Content-Type header appropriately, and that it sets the X-Content-Type-Options header to 'nosniff' for all web pages.</p><p>If possible, ensure that the end user uses a standards-compliant and modern web browser that does not perform MIME-sniffing at all, or that can be directed by the web application/web server to not perform MIME-sniffing.</p>",
+				"<p>http://msdn.microsoft.com/en-us/library/ie/gg622941%28v=vs.85%29.aspx</p><p>https://owasp.org/www-community/Security_Headers</p>"),
+		},
+		&v1.Issue{
+			Target:     "https://thisisanexample.com",
+			Type:       "16",
+			Title:      "X-Frame-Options Header Not Set",
+			Severity:   v1.Severity_SEVERITY_MEDIUM,
+			Cvss:       0.0,
+			Confidence: v1.Confidence_CONFIDENCE_MEDIUM,
+			Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n",
+				"<p>X-Frame-Options header is not included in the HTTP response to protect against 'ClickJacking' attacks.</p>",
+				"<p>Most modern Web browsers support the X-Frame-Options HTTP header. Ensure it's set on all web pages returned by your site (if you expect the page to be framed only by pages on your server (e.g. it's part of a FRAMESET) then you'll want to use SAMEORIGIN, otherwise if you never expect the page to be framed, you should use DENY. ALLOW-FROM allows specific websites to frame the web page in supported web browsers).</p>",
+				"<p>https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options</p>"),
+		},
+		&v1.Issue{
+			Target:     "https://thisithesecondexample.com",
+			Type:       "16",
+			Title:      "X-Content-Type-Options Header Missing",
+			Severity:   v1.Severity_SEVERITY_LOW,
+			Cvss:       0.0,
+			Confidence: v1.Confidence_CONFIDENCE_MEDIUM,
+			Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n",
+				"<p>The Anti-MIME-Sniffing header X-Content-Type-Options was not set to 'nosniff'. This allows older versions of Internet Explorer and Chrome to perform MIME-sniffing on the response body, potentially causing the response body to be interpreted and displayed as a content type other than the declared content type. Current (early 2014) and legacy versions of Firefox will use the declared content type (if one is set), rather than performing MIME-sniffing.</p>",
+				"<p>Ensure that the application/web server sets the Content-Type header appropriately, and that it sets the X-Content-Type-Options header to 'nosniff' for all web pages.</p><p>If possible, ensure that the end user uses a standards-compliant and modern web browser that does not perform MIME-sniffing at all, or that can be directed by the web application/web server to not perform MIME-sniffing.</p>",
+				"<p>http://msdn.microsoft.com/en-us/library/ie/gg622941%28v=vs.85%29.aspx</p><p>https://owasp.org/www-community/Security_Headers</p>"),
+		},
 	}
-    issues := parseOut(&results)
-    
-	expectedIssues := make([]*v1.Issue, 3)
-	expectedIssues[0] = &v1.Issue{
-		Target:     "https://thisisanexample.com",
-		Type:       "16",
-		Title:      "X-Content-Type-Options Header Missing",
-		Severity:   v1.Severity_SEVERITY_LOW,
-		Cvss:       0.0,
-		Confidence: v1.Confidence_CONFIDENCE_MEDIUM,
-        Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n",
-        "<p>The Anti-MIME-Sniffing header X-Content-Type-Options was not set to 'nosniff'. This allows older versions of Internet Explorer and Chrome to perform MIME-sniffing on the response body, potentially causing the response body to be interpreted and displayed as a content type other than the declared content type. Current (early 2014) and legacy versions of Firefox will use the declared content type (if one is set), rather than performing MIME-sniffing.</p>",
-        "<p>Ensure that the application/web server sets the Content-Type header appropriately, and that it sets the X-Content-Type-Options header to 'nosniff' for all web pages.</p><p>If possible, ensure that the end user uses a standards-compliant and modern web browser that does not perform MIME-sniffing at all, or that can be directed by the web application/web server to not perform MIME-sniffing.</p>",            
-        "<p>http://msdn.microsoft.com/en-us/library/ie/gg622941%28v=vs.85%29.aspx</p><p>https://owasp.org/www-community/Security_Headers</p>"),
-	}
-	expectedIssues[1] = &v1.Issue{
-		Target:     "https://thisisanexample.com",
-		Type:       "16",
-		Title:      "X-Frame-Options Header Not Set",
-		Severity:   v1.Severity_SEVERITY_MEDIUM,
-		Cvss:       0.0,
-		Confidence: v1.Confidence_CONFIDENCE_MEDIUM,
-        Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n",
-        "<p>X-Frame-Options header is not included in the HTTP response to protect against 'ClickJacking' attacks.</p>",
-        "<p>Most modern Web browsers support the X-Frame-Options HTTP header. Ensure it's set on all web pages returned by your site (if you expect the page to be framed only by pages on your server (e.g. it's part of a FRAMESET) then you'll want to use SAMEORIGIN, otherwise if you never expect the page to be framed, you should use DENY. ALLOW-FROM allows specific websites to frame the web page in supported web browsers).</p>",
-        "<p>https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options</p>"),
-	}
-	expectedIssues[2] = &v1.Issue{
-		Target:     "https://thisithesecondexample.com",
-		Type:       "16",
-		Title:      "X-Content-Type-Options Header Missing",
-		Severity:   v1.Severity_SEVERITY_LOW,
-		Cvss:       0.0,
-		Confidence: v1.Confidence_CONFIDENCE_MEDIUM,
-        Description: fmt.Sprintf("Description: %s\nSolution: %s\nReference: %s\n",
-        "<p>The Anti-MIME-Sniffing header X-Content-Type-Options was not set to 'nosniff'. This allows older versions of Internet Explorer and Chrome to perform MIME-sniffing on the response body, potentially causing the response body to be interpreted and displayed as a content type other than the declared content type. Current (early 2014) and legacy versions of Firefox will use the declared content type (if one is set), rather than performing MIME-sniffing.</p>",
-        "<p>Ensure that the application/web server sets the Content-Type header appropriately, and that it sets the X-Content-Type-Options header to 'nosniff' for all web pages.</p><p>If possible, ensure that the end user uses a standards-compliant and modern web browser that does not perform MIME-sniffing at all, or that can be directed by the web application/web server to not perform MIME-sniffing.</p>",            
-        "<p>http://msdn.microsoft.com/en-us/library/ie/gg622941%28v=vs.85%29.aspx</p><p>https://owasp.org/www-community/Security_Headers</p>"),
-    }
-    assert.Equal(t, len(expectedIssues), len(issues))
-    for i, issue := range issues {
-                assert.EqualValues(t, expectedIssues[i].Target, issue.Target)
-                assert.EqualValues(t, expectedIssues[i].Type, issue.Type)
-                assert.EqualValues(t, expectedIssues[i].Title, issue.Title)
-	 			assert.EqualValues(t, expectedIssues[i].Severity, issue.Severity)
-	 			assert.EqualValues(t, expectedIssues[i].Cvss, issue.Cvss)
-	 			assert.EqualValues(t, expectedIssues[i].Confidence, issue.Confidence)
-                assert.EqualValues(t, expectedIssues[i].Description, issue.Description)
+	assert.Equal(t, len(expectedIssues), len(issues))
+	for i, issue := range issues {
+		assert.EqualValues(t, expectedIssues[i].Target, issue.Target)
+		assert.EqualValues(t, expectedIssues[i].Type, issue.Type)
+		assert.EqualValues(t, expectedIssues[i].Title, issue.Title)
+		assert.EqualValues(t, expectedIssues[i].Severity, issue.Severity)
+		assert.EqualValues(t, expectedIssues[i].Cvss, issue.Cvss)
+		assert.EqualValues(t, expectedIssues[i].Confidence, issue.Confidence)
+		assert.EqualValues(t, expectedIssues[i].Description, issue.Description)
 	}
 
-	
 }
 
 var exampleOutput1 = `{
