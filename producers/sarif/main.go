ff7d8f1ad27dda79995b29581d3014c18d8891c1
spyros <morfeas3000@gmail.com>
2022-07-12 13:45:17 +0100

    sarif producer

--- producers/sarif/main.go
+++ producers/sarif/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"strconv"
-
-	"github.com/securego/gosec/report/sarif"
 	v1 "github.com/thought-machine/dracon/api/proto/v1"
-	"github.com/thought-machine/dracon/producers/golang_nancy/types"
 
-	"fmt"
 	"log"
 
 	"github.com/owenrumney/go-sarif/v2/sarif"
@@ -24,15 +19,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	results, err := sarif.FromString(inFile)
+	results, err := sarif.FromString(string(inFile))
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, run := range results.Runs {
 		tool := run.Tool.Driver.Name
-		if err := producers.WriteDraconOut(tool,
-			parseOut(&results),
-		); err != nil {
+		if err := producers.WriteDraconOut(tool, parseOut(*run)); err != nil {
 			log.Fatal(err)
 		}
 	}
@@ -40,54 +33,29 @@ func main() {
 
 func parseOut(run sarif.Run) []*v1.Issue {
 	issues := []*v1.Issue{}
-		for _, res := range run.Results {
-			for _, loc := range res.Locations {
-				target := loc.PhysicalLocation.ArtifactLocation.URI
-				issues = append(issues, &v1.Issue{
-					Target:      target,
-					Title:       res.RuleID,
-					Description: res.Message.Text,
-					Type: "",
-					Severity: ,
-					Confidence: ,
-					Cvss: ,
-					Cve: ,
-				})
-			}
+	for _, res := range run.Results {
+		for _, loc := range res.Locations {
+			target := loc.PhysicalLocation.ArtifactLocation.URI
+			issues = append(issues, &v1.Issue{
+				Target:      *target,
+				Title:       *res.RuleID,
+				Description: *res.Message.Text,
+				Type:        "Security Automation Result",
+				Severity:    LevelToSeverity(*res.Level),
+				Confidence:  v1.Confidence_CONFIDENCE_MEDIUM,
+				Cvss:        0,
+				Cve:         "",
+			})
 		}
+	}
 	return issues
 }
 
-func cvssToSeverity(score string) v1.Severity {
-
-	switch s, err := strconv.ParseFloat(score, 64); err == nil {
-	case 0.1 <= s && s <= 3.9:
-		return v1.Severity_SEVERITY_LOW
-	case 4.0 <= s && s <= 6.9:
-		return v1.Severity_SEVERITY_MEDIUM
-	case 7.0 <= s && s <= 8.9:
+func LevelToSeverity(level string) v1.Severity {
+	if level == "error" {
 		return v1.Severity_SEVERITY_HIGH
-	case 9.0 <= s && s <= 10.0:
-		return v1.Severity_SEVERITY_CRITICAL
-	default:
-		return v1.Severity_SEVERITY_INFO
-
+	} else if level == "warning" {
+		return v1.Severity_SEVERITY_MEDIUM
 	}
-}
-func parseResult(r *types.NancyVulnerabilities, target string) *v1.Issue {
-	cvss, err := strconv.ParseFloat(r.CvssScore, 64)
-	if err != nil {
-		cvss = 0.0
-	}
-	return &v1.Issue{
-		Target:     target,
-		Type:       "Vulnerable Dependency",
-		Title:      r.Title,
-		Severity:   cvssToSeverity(r.CvssScore),
-		Confidence: v1.Confidence_CONFIDENCE_HIGH,
-		Cvss:       cvss,
-		Description: fmt.Sprintf("CVSS Score: %v\nCvssVector: %s\nCve: %s\nCwe: %s\nReference: %s\n",
-			r.CvssScore, r.CvssVector, r.Cve, r.Cwe, r.Reference),
-		Cve: r.Cve,
-	}
+	return v1.Severity_SEVERITY_LOW
 }
--- producers/sarif/main_test.go
+++ producers/sarif/main_test.go
@@ -1,148 +1,104 @@
 package main
 
 import (
-	"fmt"
-
 	v1 "github.com/thought-machine/dracon/api/proto/v1"
-	"github.com/thought-machine/dracon/producers/golang_nancy/types"
 
-	"encoding/json"
 	"testing"
 
+	"github.com/owenrumney/go-sarif/v2/sarif"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParseOut(t *testing.T) {
-	var results types.NancyOut
-	err := json.Unmarshal([]byte(exampleOutput), &results)
+	results, err := sarif.FromString(exampleOutput)
 	if err != nil {
 		t.Logf(err.Error())
 		t.Fail()
 	}
-	issues := parseOut(&results)
 
-	expectedIssues := make([]*v1.Issue, 3)
-	expectedIssues[0] = &v1.Issue{
-		Target:      "pkg:golang/github.com/coreos/etcd@0.5.0-alpha.5",
-		Type:        "Vulnerable Dependency",
-		Title:       "[CVE-2018-1099]  Improper Input Validation",
-		Severity:    v1.Severity_SEVERITY_MEDIUM,
-		Cvss:        5.5,
-		Confidence:  v1.Confidence_CONFIDENCE_HIGH,
-		Description: fmt.Sprintf("CVSS Score: %v\nCvssVector: %s\nCve: %s\nCwe: %s\nReference: %s\n",
-			"5.5", "CVSS:3.0/AV:L/AC:L/PR:L/UI:N/S:U/C:N/I:H/A:N", "CVE-2018-1099",
-			"", "https://ossindex.sonatype.org/vuln/8a190129-526c-4ee0-b663-92f38139c165"),
-		Cve:         "CVE-2018-1099",
+	expectedIssues := []*v1.Issue{
+		&v1.Issue{
+			Target:      "main.go",
+			Type:        "Security Automation Result",
+			Title:       "G404",
+			Severity:    v1.Severity_SEVERITY_HIGH,
+			Confidence:  v1.Confidence_CONFIDENCE_MEDIUM,
+			Description: "Use of weak random number generator (math/rand instead of crypto/rand)"},
+		&v1.Issue{
+			Target:      "main.go",
+			Type:        "Security Automation Result",
+			Title:       "G104",
+			Severity:    v1.Severity_SEVERITY_MEDIUM,
+			Confidence:  v1.Confidence_CONFIDENCE_MEDIUM,
+			Description: "Errors unhandled."},
 	}
-	expectedIssues[1] = &v1.Issue{
-		Target:      "pkg:golang/github.com/coreos/etcd@0.5.0-alpha.5",
-		Type:        "Vulnerable Dependency",
-		Title:       "[CVE-2018-1098]  Cross-Site Request Forgery (CSRF)",
-		Severity:    v1.Severity_SEVERITY_HIGH,
-		Cvss:        8.8,
-		Confidence:  v1.Confidence_CONFIDENCE_HIGH,
-		Description: fmt.Sprintf("CVSS Score: %v\nCvssVector: %s\nCve: %s\nCwe: %s\nReference: %s\n",
-			"8.8", "CVSS:3.0/AV:N/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:H", "CVE-2018-1098",
-			"", "https://ossindex.sonatype.org/vuln/5c876f5e-2814-4822-baf0-1092fc63ec25"),
-		Cve:         "CVE-2018-1098",
-	}
-	expectedIssues[2] = &v1.Issue{
-		Target:     "pkg:golang/github.com/gorilla/websocket@1.2.0",
-		Type:       "Vulnerable Dependency",
-		Title:      "CWE-190: Integer Overflow or Wraparound",
-		Severity:   v1.Severity_SEVERITY_HIGH,
-		Cvss:       7.5,
-		Confidence: v1.Confidence_CONFIDENCE_HIGH,
-		Description: fmt.Sprintf("CVSS Score: %v\nCvssVector: %s\nCve: %s\nCwe: %s\nReference: %s\n",
-			"7.5", "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:H", "",
-			"CWE-190", "https://ossindex.sonatype.org/vuln/5f259e63-3efb-4c47-b593-d175dca716b0"),
-	}
+	for _, run := range results.Runs {
+		issues := parseOut(*run)
 
-	found := 0
-	assert.Equal(t, len(expectedIssues), len(issues))
-	for _, issue := range issues {
-		singleMatch := 0
-		for _, expected := range expectedIssues {
-			if expected.Title == issue.Title {
-				singleMatch++
-				found++
-				assert.Equal(t, singleMatch, 1) //assert no duplicates
-				assert.EqualValues(t, expected.Type, issue.Type)
-				assert.EqualValues(t, expected.Title, issue.Title)
-				assert.EqualValues(t, expected.Severity, issue.Severity)
-				assert.EqualValues(t, expected.Cvss, issue.Cvss)
-				assert.EqualValues(t, expected.Confidence, issue.Confidence)
-				assert.EqualValues(t, expected.Description, issue.Description)
-				assert.EqualValues(t, expected.Cve, issue.Cve)
-			}
-		}
+		assert.Equal(t, expectedIssues, issues)
 	}
-	assert.Equal(t, found, len(issues)) //assert everything has been found
 }
 
 var exampleOutput = `{
-	"audited": [
-	  {
-		"Coordinates": "pkg:golang/4d63.com/embedfiles@0.0.0-20190311033909-995e0740726f",
-		"Reference": "https://ossindex.sonatype.org/component/pkg:golang/4d63.com/embedfiles@0.0.0-20190311033909-995e0740726f",
-		"Vulnerabilities": [],
-		"InvalidSemVer": false
-	  }
-	],
-	"exclusions": [],
-	"invalid": [],
-	"num_audited": 679,
-	"num_vulnerable": 6,
-	"version": "0.3.1",
-	"vulnerable": [
-	  {
-		"Coordinates": "pkg:golang/github.com/coreos/etcd@0.5.0-alpha.5",
-		"Reference": "https://ossindex.sonatype.org/component/pkg:golang/github.com/coreos/etcd@0.5.0-alpha.5",
-		"Vulnerabilities": [
-		  {
-			"Id": "8a190129-526c-4ee0-b663-92f38139c165",
-			"Title": "[CVE-2018-1099]  Improper Input Validation",
-			"Description": "DNS rebinding vulnerability found in etcd 3.3.1 and earlier. An attacker can control his DNS records to direct to localhost, and trick the browser into sending requests to localhost (or any other address).",
-			"CvssScore": "5.5",
-			"CvssVector": "CVSS:3.0/AV:L/AC:L/PR:L/UI:N/S:U/C:N/I:H/A:N",
-			"Cve": "CVE-2018-1099",
-			"Cwe": "",
-			"Reference": "https://ossindex.sonatype.org/vuln/8a190129-526c-4ee0-b663-92f38139c165",
-			"Excluded": false
-		  },
-		  {
-			"Id": "5c876f5e-2814-4822-baf0-1092fc63ec25",
-			"Title": "[CVE-2018-1098]  Cross-Site Request Forgery (CSRF)",
-			"Description": "A cross-site request forgery flaw was found in etcd 3.3.1 and earlier. An attacker can set up a website that tries to send a POST request to the etcd server and modify a key. Adding a key is done with PUT so it is theoretically safe (can't PUT from an HTML form or such) but POST allows creating in-order keys that an attacker can send.",
-			"CvssScore": "8.8",
-			"CvssVector": "CVSS:3.0/AV:N/AC:L/PR:N/UI:R/S:U/C:H/I:H/A:H",
-			"Cve": "CVE-2018-1098",
-			"Cwe": "",
-			"Reference": "https://ossindex.sonatype.org/vuln/5c876f5e-2814-4822-baf0-1092fc63ec25",
-			"Excluded": false
-		  }
+	"runs": [{
+		"results": [{
+				"level": "error",
+				"locations": [{
+					"physicalLocation": {
+						"artifactLocation": {
+							"uri": "main.go"
+						},
+						"region": {
+							"endColumn": 7,
+							"endLine": 83,
+							"snippet": {
+								"text": "r := rand.New(rand.NewSource(time.Now().UnixNano()))"
+							},
+							"sourceLanguage": "go",
+							"startColumn": 7,
+							"startLine": 83
+						}
+					}
+				}],
+				"message": {
+					"text": "Use of weak random number generator (math/rand instead of crypto/rand)"
+				},
+				"ruleId": "G404"
+			},
+			{
+				"level": "warning",
+				"locations": [{
+					"physicalLocation": {
+						"artifactLocation": {
+							"uri": "main.go"
+						},
+						"region": {
+							"endColumn": 2,
+							"endLine": 347,
+							"snippet": {
+								"text": "zipWriter.Close()"
+							},
+							"sourceLanguage": "go",
+							"startColumn": 2,
+							"startLine": 347
+						}
+					}
+				}],
+				"message": {
+					"text": "Errors unhandled."
+				},
+				"ruleId": "G104",
+				"ruleIndex": 3
+			}
 		],
-		"InvalidSemVer": false
-	  },
-	  {
-		"Coordinates": "pkg:golang/github.com/gorilla/websocket@1.2.0",
-		"Reference": "https://ossindex.sonatype.org/component/pkg:golang/github.com/gorilla/websocket@1.2.0",
-		"Vulnerabilities": [
-		  {
-			"Id": "5f259e63-3efb-4c47-b593-d175dca716b0",
-			"Title": "CWE-190: Integer Overflow or Wraparound",
-			"Description": "The software performs a calculation that can produce an integer overflow or wraparound, when the logic assumes that the resulting value will always be larger than the original value. This can introduce other weaknesses when the calculation is used for resource management or execution control.",
-			"CvssScore": "7.5",
-			"CvssVector": "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:H",
-			"Cve": "",
-			"Cwe": "CWE-190",
-			"Reference": "https://ossindex.sonatype.org/vuln/5f259e63-3efb-4c47-b593-d175dca716b0",
-			"Excluded": false
-		  }
-		],
-		"InvalidSemVer": false
-	  }
-
-	]
-  }
-`
+		"tool": {
+			"driver": {
+				"guid": "8b518d5f-906d-39f9-894b-d327b1a421c5",
+				"informationUri": "https://github.com/securego/gosec/",
+				"name": "gosec"
+			}
+		}
+	}],
+	"$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
+	"version": "2.1.0"
+}`
