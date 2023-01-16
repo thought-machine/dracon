75f439149e765b3791d7a9370049bf91eddb470a
mbardsley <mbardsley@thoughtmachine.net>
2023-01-16 16:46:23 +0000

    Add remaining tests

---
+++ pkg/template/load_test.go
@@ -0,0 +1,102 @@
+package template
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplitYAML(t *testing.T) {
+	var tests = []struct {
+		desc     string
+		inBytes  []byte
+		outBytes [][]byte
+		err      error
+	}{
+		{
+			desc: "a simple YAML file with no leading document separator",
+			inBytes: []byte(`a: Easy
+b:
+    c: 2
+    d:
+        - 3
+        - 4
+`),
+			outBytes: [][]byte{[]byte(`a: Easy
+b:
+    c: 2
+    d:
+        - 3
+        - 4
+`)},
+			err: nil,
+		},
+		{
+			desc: "a simple YAML file with a leading document separator",
+			inBytes: []byte(`---
+a: Easy
+b:
+    c: 2
+    d:
+        - 3
+        - 4
+`),
+			outBytes: [][]byte{[]byte(`a: Easy
+b:
+    c: 2
+    d:
+        - 3
+        - 4
+`)},
+			err: nil,
+		},
+		{
+			desc: "a YAML file with no leading document separator but a string value containing ---",
+			inBytes: []byte(`a: Easy
+b:
+    c: 2 ---
+    d:
+        - 3
+        - 4
+`),
+			outBytes: [][]byte{[]byte(`a: Easy
+b:
+    c: 2 ---
+    d:
+        - 3
+        - 4
+`)},
+			err: nil,
+		},
+		{
+			desc: "a YAML file with both a leading document separator and a string value containing ---",
+			inBytes: []byte(`---
+a: Easy
+b:
+    c: 2 ---
+    d:
+        - 3
+        - 4
+`),
+			outBytes: [][]byte{[]byte(`a: Easy
+b:
+    c: 2 ---
+    d:
+        - 3
+        - 4
+`)},
+			err: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			outBytes, err := splitYAML(tt.inBytes)
+			if tt.err == nil {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+			assert.Equal(t, tt.outBytes, outBytes)
+		})
+	}
+}
